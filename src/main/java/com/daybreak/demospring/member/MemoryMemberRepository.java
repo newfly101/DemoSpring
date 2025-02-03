ac7bd59aa8679f2a0b88555622d2fe1aa46cf414
Jae-Hong-Kim <newfly101@naver.com>
2025-02-03 10:53:34 +0900

    [JAVA] 회원도메인 설계 기초 1 - 메모리용 Repository rel #2

---
+++ src/main/java/com/daybreak/demospring/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package com.daybreak.demospring.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+    // DB 확정이 되지 않아서 Memory용 test용으로만 사용
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // 동시성 문제를 고려한다면 ConcurrentHashMap<>() 을 사용해야 한다.
+
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}

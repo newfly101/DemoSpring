dfd9afd001ac6562653d9101a323afaeee7b1bcc
Jae-Hong-Kim <newfly101@naver.com>
2025-02-03 13:20:15 +0900

    [JAVA] 신규 할인 정책 추가 rel #10

---
+++ src/main/java/com/daybreak/demospring/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.daybreak.demospring.discount;
+
+import com.daybreak.demospring.member.Grade;
+import com.daybreak.demospring.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * (100 - discountPercent) / 100;
+        } else {
+            return 0;
+        }
+    }
+}

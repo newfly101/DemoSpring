1f57bad5e0224c4014cdd956f412f2b156ff9f26
Jae-Hong-Kim <newfly101@naver.com>
2025-02-03 13:03:56 +0900

    [JAVA] 주문 및 할인정책 테스트 rel #9

---
+++ src/test/java/com/daybreak/demospring/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package com.daybreak.demospring.order;
+
+import com.daybreak.demospring.member.Grade;
+import com.daybreak.demospring.member.Member;
+import com.daybreak.demospring.member.MemberService;
+import com.daybreak.demospring.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}

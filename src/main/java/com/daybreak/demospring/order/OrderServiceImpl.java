bbc824820fe0fd1be967beab755854b5420c480a
Jae-Hong-Kim <newfly101@naver.com>
2025-02-03 15:43:17 +0900

    [JAVA] 신규 할인 정책 추가 - 문제점 발생 rel #12

--- src/main/java/com/daybreak/demospring/order/OrderServiceImpl.java
+++ src/main/java/com/daybreak/demospring/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.daybreak.demospring.order;
 
 import com.daybreak.demospring.discount.DiscountPolicy;
 import com.daybreak.demospring.discount.FixDiscountPolicy;
+import com.daybreak.demospring.discount.RateDiscountPolicy;
 import com.daybreak.demospring.member.Member;
 import com.daybreak.demospring.member.MemberRepository;
 import com.daybreak.demospring.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import com.daybreak.demospring.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // TODO : 문제점 => OCP를 위반
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

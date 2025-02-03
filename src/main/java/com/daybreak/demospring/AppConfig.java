11843c7da098d2c35c3ebe96155a69eb6d82cff6
Jae-Hong-Kim <newfly101@naver.com>
2025-02-03 16:33:36 +0900

    [JAVA] 외부 주입 리팩토링 - 관심사 분리 rel #13

--- src/main/java/com/daybreak/demospring/AppConfig.java
+++ src/main/java/com/daybreak/demospring/AppConfig.java
@@ -1,6 +1,8 @@
 package com.daybreak.demospring;
 
+import com.daybreak.demospring.discount.DiscountPolicy;
 import com.daybreak.demospring.discount.FixDiscountPolicy;
+import com.daybreak.demospring.member.MemberRepository;
 import com.daybreak.demospring.member.MemberService;
 import com.daybreak.demospring.member.MemberServiceImpl;
 import com.daybreak.demospring.member.MemoryMemberRepository;
@@ -8,11 +10,21 @@ import com.daybreak.demospring.order.OrderService;
 import com.daybreak.demospring.order.OrderServiceImpl;
 
 public class AppConfig {
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    };
+        return new MemberServiceImpl(memberRepository());
+    }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
 }

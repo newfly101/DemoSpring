ae39cd3d3c90bed35fd63892db5ddd58d6e49604
Jae-Hong-Kim <newfly101@naver.com>
2025-02-04 16:06:47 +0900

    [SPRING] @Configuration과 싱글톤 - 싱글톤이 되지 않고 각각 다르게 나오는 문제점 발생

--- src/main/java/com/daybreak/demospring/member/MemberServiceImpl.java
+++ src/main/java/com/daybreak/demospring/member/MemberServiceImpl.java
@@ -18,4 +18,9 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    // TEST 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
---
+++ src/test/java/com/daybreak/demospring/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,29 @@
+package com.daybreak.demospring.singleton;
+
+import com.daybreak.demospring.AppConfig;
+import com.daybreak.demospring.member.MemberRepository;
+import com.daybreak.demospring.member.MemberServiceImpl;
+import com.daybreak.demospring.order.OrderServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    @DisplayName("Configuration 테스트")
+    void configurationSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+    }
+}

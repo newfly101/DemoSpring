7ddba821e4c3fbb981becae33ac5548d602feeb2
Jae-Hong-Kim <newfly101@naver.com>
2025-02-12 00:35:10 +0900

    [SPRING] 다양한 의존관계 주입 - 필드 주입 : 설정 부분에서 불러올때 이용 하는 예제

--- src/main/java/com/daybreak/demospring/AutoAppConfig.java
+++ src/main/java/com/daybreak/demospring/AutoAppConfig.java
@@ -1,7 +1,11 @@
 package com.daybreak.demospring;
 
+import com.daybreak.demospring.discount.DiscountPolicy;
 import com.daybreak.demospring.member.MemberRepository;
 import com.daybreak.demospring.member.MemoryMemberRepository;
+import com.daybreak.demospring.order.OrderService;
+import com.daybreak.demospring.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +18,15 @@ import static org.springframework.context.annotation.ComponentScan.*;
         basePackages = "com.daybreak.demospring.member",
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired DiscountPolicy discountPolicy;
+
+    @Bean
+    OrderService orderService() {
+        return new OrderServiceImpl(memberRepository, discountPolicy);
+    }
+
     @Bean(name = "memoryMemberRepository") // 수동 빈 등록
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();

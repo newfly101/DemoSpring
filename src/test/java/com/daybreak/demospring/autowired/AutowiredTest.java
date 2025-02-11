22a4c5966ef7252964ba5354c2b1c9a4dfe05aee
Jae-Hong-Kim <newfly101@naver.com>
2025-02-12 01:07:11 +0900

    [SPRING] 옵션 처리

---
+++ src/test/java/com/daybreak/demospring/autowired/AutowiredTest.java
@@ -0,0 +1,33 @@
+package com.daybreak.demospring.autowired;
+
+import com.daybreak.demospring.member.Member;
+import io.micrometer.common.lang.Nullable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+    @Test
+    @DisplayName("자동주입테스트1")
+    void AutowiredOption() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}

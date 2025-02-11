b95b091c1a69195527b2991b4984d189144fdfe3
Jae-Hong-Kim <newfly101@naver.com>
2025-02-12 02:21:14 +0900

    [SPRING] 조회한 빈이 모두 필요할 때 - List, Map 으로 TEST 코드 추가

--- src/test/java/com/daybreak/demospring/autowired/AllBeanTest.java
+++ src/test/java/com/daybreak/demospring/autowired/AllBeanTest.java
@@ -2,6 +2,9 @@ package com.daybreak.demospring.autowired;
 
 import com.daybreak.demospring.AutoAppConfig;
 import com.daybreak.demospring.discount.DiscountPolicy;
+import com.daybreak.demospring.member.Grade;
+import com.daybreak.demospring.member.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,26 +13,40 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AllBeanTest {
 
     @Test
     @DisplayName("모든 빈 찾기")
     void findAllBean() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L , "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
     }
     static class DiscountService {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policyList;
 
-        @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
             this.policyMap = policyMap;
             this.policyList = policyList;
             System.out.println("policyMap = " + policyMap);
             System.out.println("policyList = " + policyList);
         }
-    }
 
-    public AllBeanTest() {
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
     }
 }
